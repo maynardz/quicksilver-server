e9204066e09f8968ef5c51b1094bfd224fff138a
maynardz <maynard.zach23@gmail.com>
2021-03-17 14:16:09 -0400

    test deploy new ssl

--- server/config/db.js
+++ server/config/db.js
@@ -3,12 +3,7 @@ const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize(env.DATABASE_URL, {
     dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false
-        }
-    }
+    ssl: env.NODE_ENV === 'production',
 });
 
 module.exports = sequelize;
