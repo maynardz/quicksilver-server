60cec906ccda0ebab1cdf5f3304307d250d6c60f
maynardz <maynard.zach23@gmail.com>
2021-03-17 13:09:27 -0400

    reject unauth: true

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,12 @@ const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize(env.DATABASE_URL, {
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: true
+        }
+    }
 });
 
 module.exports = sequelize;
