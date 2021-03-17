c121342a8d07b279e550e5032c13e98f7ec8ca26
maynardz <maynard.zach23@gmail.com>
2021-03-17 13:07:26 -0400

    test db

--- server/config/db.js
+++ server/config/db.js
@@ -3,12 +3,6 @@ const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize(env.DATABASE_URL, {
     dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false
-        }
-    }
 });
 
 module.exports = sequelize;
