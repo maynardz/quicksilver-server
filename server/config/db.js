0fb363b6af56e29d0e0c5ed382b3e261a367ef7e
maynardz <maynard.zach23@gmail.com>
2021-03-17 14:29:14 -0400

    ssl: true and PGSSLMODE no verify

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,6 @@ const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize(env.DATABASE_URL, {
     dialect: 'postgres',
-    ssl: env.NODE_ENV === 'production',
-    dialectOptions: {
-        ssl: env.NODE_ENV === 'production'
-    }
 });
 
 module.exports = sequelize;
