9694247e9a586964cf89feba189fe5379fd83c1b
maynardz <maynard.zach23@gmail.com>
2021-03-17 14:19:52 -0400

    test dialectOptions

--- server/config/db.js
+++ server/config/db.js
@@ -4,6 +4,9 @@ const {Sequelize} = require('sequelize');
 const sequelize = new Sequelize(env.DATABASE_URL, {
     dialect: 'postgres',
     ssl: env.NODE_ENV === 'production',
+    dialectOptions: {
+        ssl: env.NODE_ENV === 'production'
+    }
 });
 
 module.exports = sequelize;
