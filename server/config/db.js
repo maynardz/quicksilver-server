2f64a730229ce0df5d9619469e32cf02896125e8
maynardz <maynard.zach23@gmail.com>
2021-03-17 15:55:21 -0400

    deployed db fix

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,7 @@ const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize(env.DATABASE_URL, {
     dialect: 'postgres',
+    ssl: env.NODE_ENV === 'production'
 });
 
 module.exports = sequelize;
