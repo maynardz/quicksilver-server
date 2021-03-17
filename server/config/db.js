e39e3e2b367d7ab94c0fc1f2aec8edc65f53e140
maynardz <maynard.zach23@gmail.com>
2021-03-17 13:12:00 -0400

    unauth back to true

--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(env.DATABASE_URL, {
     dialectOptions: {
         ssl: {
           require: true,
-          rejectUnauthorized: true
+          rejectUnauthorized: false
         }
     }
 });
